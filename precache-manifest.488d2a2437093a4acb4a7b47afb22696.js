81ea319be10687b8191c65602804ffab817d9b66
James Lemire <webdev.jameslemire@gmail.com>
2019-01-02 12:39:12 -0700

    Updates

---
+++ precache-manifest.488d2a2437093a4acb4a7b47afb22696.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "922c9b5e0c79bc1a9e24",
+    "url": "/gh-pages-routing/static/js/runtime~main.922c9b5e.js"
+  },
+  {
+    "revision": "b588c7103ca9dee8db41",
+    "url": "/gh-pages-routing/static/js/main.b588c710.chunk.js"
+  },
+  {
+    "revision": "21b017edb0fa6b8912af",
+    "url": "/gh-pages-routing/static/js/1.21b017ed.chunk.js"
+  },
+  {
+    "revision": "b588c7103ca9dee8db41",
+    "url": "/gh-pages-routing/static/css/main.277fa7c6.chunk.css"
+  },
+  {
+    "revision": "ebe4fc94d1a435d913ddff6c9e7b5733",
+    "url": "/gh-pages-routing/index.html"
+  }
+];
