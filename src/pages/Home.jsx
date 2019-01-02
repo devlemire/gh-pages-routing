b9a2e05905fae449cd81972636444371ab609001
James Lemire <webdev.jameslemire@gmail.com>
2019-01-02 13:00:45 -0700

    JL master - Use presentational components.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Nav from '../components/Nav'
 
-export default class Home extends React.Component {
-  render() {
-    return (
-      <section id="Home-container">
-        <h1>I am Home</h1>
-        <Nav />
-      </section>
-    )
-  }
+export default function Home() {
+  return (
+    <section id="Home-container">
+      <h1>I am Home</h1>
+      <Nav />
+    </section>
+  )
 }
--- src/pages/Route1.jsx
+++ src/pages/Route1.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Nav from '../components/Nav'
 
-export default class Route1 extends React.Component {
-  render() {
-    return (
-      <section id="Route1-container">
-        <h1>I am Route1</h1>
-        <Nav />
-      </section>
-    )
-  }
+export default function Route1() {
+  return (
+    <section id="Route1-container">
+      <h1>I am Route1</h1>
+      <Nav />
+    </section>
+  )
 }
